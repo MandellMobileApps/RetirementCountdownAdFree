77db246aca841a735c77c0eb941f5b60516f870a
Jon Mandell <Jon@MandellMobileApps.com>
2020-03-20 11:02:57 -0700

    fixed TimeRemaining.h    now idea how it got screwed up

---
+++ Classes/TimeRemainingNew.h
@@ -0,0 +1,20 @@
+//
+//  TimeRemaining.h
+//  RetirementCountdown
+//
+//  Created by Jon Mandell on 11/7/09.
+//  Copyright 2009 MandellMobileApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TimeRemainingNew : NSObject {
+
+
+}
+
+
+-(NSMutableArray*)getTimeRemaining;
+-(void) totalWorkdaysTilRetirement;
+@end
